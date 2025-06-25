466a57b551de7a0c8c47587b2d0724ed3b5c03e2
yosrabenchikha <yossra.benchikha@gmail.com>
2025-06-25 16:27:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from prophet import Prophet
 from prophet.plot import plot_plotly, plot_components_plotly
 import plotly.graph_objs as go
 from datetime import datetime
